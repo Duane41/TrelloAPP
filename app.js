39a3a733174f761d7afe3b225d4f8b2fd50ea3fd
Duane41 <duane@kopano.ai>
2021-03-09 16:02:21 +0200

    added route for create card

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
 
-const toDo = require("./utils/getCards");
+const getCards = require("./utils/getCards");
+const createCard = require("./utils/createCard");
 
 const app = express();
 
 const port = 5000;
 
 app.get('/api/getToDo', (req, res) => {
-    toDo((error, lists) => {
+    getCards((error, lists) => {
+        res.json(lists)
+    });
+});
+
+app.get('/api/createCard', (req, res) => {
+    let { name, list_id } = req.query
+    createCard((error, lists) => {
         res.json(lists)
     });
 });
